bd7a202d498d121d4362f00209e80d0ed408a1a4
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2025-01-11 23:17:16 +0700

    dev: deliver banh tet properties

--- src/Components/BanhTetProperties/BanhTetProperties.tsx
+++ src/Components/BanhTetProperties/BanhTetProperties.tsx
@@ -2,7 +2,7 @@ import { PropertiesTabs } from "./PropertiesTabs";
 
 export const BanhTetProperties = () => {
     return (
-        <div className="">
+        <div className="bg-[#0e540a] w-[100vw] h-[100vh] place-items-center content-center">
             <PropertiesTabs />
         </div>
     );
