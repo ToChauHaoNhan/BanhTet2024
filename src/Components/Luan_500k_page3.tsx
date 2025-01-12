ac79ec54a7d8915a65b6586d6c13b8dda4a072a7
ToChauHaoNhan <tochauhaonhan0812@gmail.com>
2025-01-12 22:45:45 +0700

    fix css

--- src/Components/Luan_500k_page3.tsx
+++ src/Components/Luan_500k_page3.tsx
@@ -16,7 +16,7 @@ export const Luan_500k_page3 = () => {
           nha!
         </div>
         <br />
-        <a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className = "text-black text-xl bg-white rounded-full p-4 w-fit hover:opacity-80">Qua Facebook sốp</a>
+        <a href = "https://www.facebook.com/tothihaohanh3" target="_blank" className = "text-black text-xl bg-white rounded-full p-4 w-fit hover:opacity-80">Qua Facebook sốp</a>
       </div>
       <div className = "flex flex-col justify-center items-center">
         <img className="max-h-[500px]" src={Image} />
