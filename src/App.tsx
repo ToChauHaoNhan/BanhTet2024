62c9ea3db53505778a8b68bd8a31688b11aa6e81
Phạm Trí Nhân <114942828+TriNhan-Pham@users.noreply.github.com>
2025-01-12 13:57:33 +1000

    Tri Nhan 500k

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { Luan_500k_page4 } from './Components/Luan_500k_page4';
 // import { Luan_500k_page3 } from './Components/Luan_500k_page3';
 import { TestPopup } from './Page/TestPopup';
 import { BanhTetProperties } from './Components/BanhTetProperties/BanhTetProperties';
+import { TriNhan500k } from './Components/TriNhan500k/BanhTetProperties';
+
 function App() {
   const [finalForm, setFinalForm] = React.useState({})
   return (
@@ -18,6 +20,7 @@ function App() {
           {/* <Route path="/3" element={<Luan_500k_page3 />} /> */}
           <Route path="/pop-up" element={<TestPopup finalForm = {finalForm} setFinalForm = {setFinalForm}/>} />
           <Route path="/banhtet-properties" element={<BanhTetProperties />} />
+          <Route path="/tri-nhan-500k" element={<TriNhan500k />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/TriNhan500k/BanhTetProperties.tsx
@@ -0,0 +1,8 @@
+
+export const TriNhan500k = () => {
+    return (
+        <div className="bg-[#0e540a] w-[100vw] h-[100vh] place-items-center content-center">
+            
+        </div>
+    );
+}
