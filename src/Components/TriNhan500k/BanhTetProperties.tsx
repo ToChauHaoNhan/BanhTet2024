70f2967517304f3963a675b61cbfb27c801434f2
Phạm Trí Nhân <114942828+TriNhan-Pham@users.noreply.github.com>
2025-01-12 14:48:25 +1000

    landing added

--- src/Components/TriNhan500k/BanhTetProperties.tsx
+++
@@ -1,8 +0,0 @@
-
-export const TriNhan500k = () => {
-    return (
-        <div className="bg-[#0e540a] w-[100vw] h-[100vh] place-items-center content-center">
-            
-        </div>
-    );
-}
