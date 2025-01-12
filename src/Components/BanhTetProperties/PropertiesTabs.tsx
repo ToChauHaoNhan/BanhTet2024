ee360cf897ec371a34b84d593a35573b30c1af72
ToChauHaoNhan <tochauhaonhan0812@gmail.com>
2025-01-12 22:28:56 +0700

    merge branch

--- src/Components/BanhTetProperties/PropertiesTabs.tsx
+++ src/Components/BanhTetProperties/PropertiesTabs.tsx
@@ -58,6 +58,11 @@ export const TabsContent = ({
                 ngaychon: day[active_day],
               },
             });
+            console.log({
+              ...propertiesForm,
+              ngaychon: day[active_day],
+            });
+            console.log(finalForm);
             setShow(!show);
           }}
         >
@@ -255,7 +260,6 @@ export const PropertiesTabs = ({
             <Luan_500k_page1
               finalForm={finalForm}
               setFinalForm={setFinalForm}
-              setShow = {setShow}
             />
           </motion.div>
         ) : null}
